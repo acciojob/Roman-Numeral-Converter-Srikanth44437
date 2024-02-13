a5c86fcd62a785e80b49818fe3844d2c603b4088
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-13 14:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+    let result = '';
+
+  for (let symbol in obj) {
+    while (num >= obj[symbol]) {
+      result += symbol;
+      num -= obj[symbol];
+    }
+  }
+
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+//console.log(convertToRoman(36));
 
 
 
